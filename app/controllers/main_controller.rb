6e4a61bbc09203c4f806fe663fbc05cbc18b6d34
b.gabriel.ma.dev@gmail.com <b.gabriel.ma.dev@gmail.com>
2021-04-11 18:29:14 +0100

    feat: created tweet, twitter_account and the remaing logic

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -1,7 +1,2 @@
 class MainController < ApplicationController
-    def index
-        #.now to prevent being written in cache and maintained to the current page
-        flash[:notice] = "Logged in successfully"
-        flash[:alert] = "Invalid email or password"
-    end
 end 
